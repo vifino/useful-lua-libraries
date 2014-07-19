a0198c0564bc5b7442836a1d24d9e4c8bba35ffd
vifino <adinator1999@gmail.com>
2014-07-19 02:32:09 +0200

    First basic, but functional version of forth.lua

--- forth.lua
+++ forth.lua
@@ -2,20 +2,62 @@
 -- Made by vifino
 forth = {}
 forth.stack = {}
-forth.words = {}
+forth.userwords = {}
+function forth.push(val)
+	table.insert(forth.stack, tostring(val))
+	return tostring(val)
+end
+function forth.pop()
+    return table.remove(forth.stack)
+end
+function forth.peek(n)
+    return forth.stack[#forth.stack-(n or 0)]
+end
+local push=forth.push
+local pop=forth.pop
+local peek=forth.peek
+forth.keywords = {
+	["+"] = function() push(pop()+pop()) end,
+	["-"] = function() push(-pop()+pop()) end,
+	["*"] = function() push(pop()*pop()) end,
+	["*"] = function() push(1/pop()*pop()) end,
+	["%"] = function() push(math.mod(pop(),pop())) end,
+	["*"] = function() push(pop()^pop()) end,
+	["PRINT"]=function() return pop() end,
+	["DUP"]=function() push(push(pop())) end
+}
+function forth.push(val)
+	table.insert(forth.stack, tostring(val))
+end
+function forth.pop()
+    return table.remove(forth.stack)
+end
+function forth.peek(n)
+    return forth.stack[#forth.stack-n]
+end
 function forth.evalInstruction(instruction)
-	
+	if forth.keywords[instruction] then
+		return forth.keywords[instruction]()
+	elseif forth.userwords[instruction] then
+		return forth.userwords[instruction]()
+	elseif tonumber(instruction) then
+		push(tostring(tonumber(instruction))) -- Err... yeah...
+	else
+		error("UNKNOWN WORD")
+	end
 end
 function forth.eval(instructionsText)
 	local instructions = {}
-	for word in instructionsText:gmatch(seperator) do table.insert(instructions, word) end
+	for word in instructionsText:gmatch("%S+") do table.insert(instructions, word) end
 	local o = ""
 	for i,instruction in pairs(instructions) do
 		--o = o ..((forth.evalInstruction(instruction) or "")
 		o=o..(function(inst)
 			local out = forth.evalInstruction(inst)
-			if out then out=tostring(out).."\n" return out end
+			if out then out=tostring(out) end
+			return out or ""
 		end)(instruction)
 	end
 	return o or ""
 end
+print(forth.eval("1 DUP + PRINT"))
